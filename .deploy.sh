503ffc0eda8b24eb115798a4ee9f346c3e4a02e7
marqhack <marqhack@gmail.com>
2016-11-28 14:26:21 -0500

    changed github handle

--- .deploy.sh
+++ .deploy.sh
@@ -19,4 +19,4 @@ git add .
 git commit -m ":shipit: Deploy to GitHub Pages"
 
 # Force push to gh-pages
-git push --force "https://${GH_TOKEN}@github.com/tteltrab/testing-demo.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/marqhack/testing-demo.git" master:gh-pages > /dev/null 2>&1
